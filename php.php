72ec10326b1fd484eb6e038c96ed7667b6a7272c
ike-njoku <31851617+ike-njoku@users.noreply.github.com>
2020-02-23 21:24:50 +0100

    Add files via upload

--- php.php
+++ php.php
@@ -57,8 +57,31 @@
 ?>
 
 <?php
-	$lists = "123456";
-	$a =str_split($lists);
-	end($a);
-	echo current($a);
+// Function to get the client IP address
+function get_client_ip() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+
+    echo $ipaddress;
+    return $ipaddress;
+
+}
+
+
+get_client_ip();
+
+
 ?>
