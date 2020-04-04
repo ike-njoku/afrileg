26267f85a3f10aceef65ac399f2ab2f6ea503bc0
ike-njoku <31851617+ike-njoku@users.noreply.github.com>
2020-04-04 12:32:13 -0700

    Add files via upload

--- include/header.php
+++ include/header.php
@@ -96,8 +96,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Afrileg.com</title>
-	<link rel="icon" href="images/favicon.png">
+	<title>Afriduds African Market</title>
+	<link rel="icon" href="images/favicon2.png">
 	<meta charset="utf8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
