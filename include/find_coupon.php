70f023999a91cf3d2430cba94b0d5ace2a861798
ike-njoku <31851617+ike-njoku@users.noreply.github.com>
2020-04-18 18:47:34 -0700

    Add files via upload

--- include/find_coupon.php
+++ include/find_coupon.php
@@ -4,8 +4,11 @@
 
 	if (isset($_SESSION['id'])) {
 		$customer_id = $_SESSION['id'];
-
-
+		$customer_type ='customer';
+	}else{
+		$customer_id = $_COOKIE['guest_id'];
+		$customer_type = "guest";
+	}
 	 	// get the variables
 		
 		if (isset($_GET['key_'])) {
@@ -17,12 +20,18 @@
 				return;
 			}
 
-			$get_coupon = mysqli_query($config,"select * from coupons where customer_id='$customer_id' and used='0' and code='$key_' ");
-			$coupon = mysqli_fetch_assoc($get_coupon);
+			$get_coupon = mysqli_query($config,"select * from coupons where customer_id='$customer_id' and used='0' and code='$key_' and customer_type ='$customer_type' ");
+			if(mysqli_num_rows($get_coupon))
+				{
+				$coupon = mysqli_fetch_assoc($get_coupon);
 
-			echo '<b>-N</b>'.$coupon['value'];
+				echo '<b>-N</b>'.$coupon['value'];
+			}else{
+				echo "<span class ='small'>...coupon not found</span>";
+			}
+			
 			
 	
 		}
-	}
+	
 ?>
